61262d5fc4707aaf7e863fc994f2fc38d28ee8bc
JAbozarOid <araghibdoust@hotmail.com>
2020-09-07 12:32:17 +0430

    parse_json_accessToken

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,7 +1,9 @@
 // this class uses for make requests
+import 'dart:convert';
+
 import 'package:flutter_rest_api/app/services/api.dart';
-// we use 'as' in import of http for using 'http' as a seperate method for using post request in getAccessToken method 
-// if you remove as the http.post is unknown in method getAccessToken method  
+// we use 'as' in import of http for using 'http' as a seperate method for using post request in getAccessToken method
+// if you remove as the http.post is unknown in method getAccessToken method
 import 'package:http/http.dart' as http;
 
 // use this class for making requests
@@ -15,5 +17,19 @@ class APIService {
   Future<String> getAccessToken() async {
     final response = await http.post(api.tokenUri().toString(),
         headers: {'Authorization': 'Basic ${api.apiKey}'});
+    if (response.statusCode == 200) {
+      // because of the response in json format we must decode the json body (get String object)
+      // decode method parse the json in input string and return a key value pairs
+      final data = json.decode(response.body);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+
+    print(
+        'Request ${api.tokenUri()} failed\nResponse: ${response.statusCode} ${response.reasonPhrase}');
+    // if there was an error it must be handled in presentation and UI layer
+    throw response;
   }
 }
