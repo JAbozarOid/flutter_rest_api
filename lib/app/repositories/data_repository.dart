682ea89ce79bb7c4ae858382c588e1204ba5bcc3
JAbozarOid <araghibdoust@hotmail.com>
2020-09-08 14:16:20 +0430

    refresh_access_token

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_rest_api/app/services/api.dart';
 import 'package:flutter_rest_api/app/services/api_service.dart';
+import 'package:http/http.dart';
 
 class DataRepository {
   final APIService apiService;
+
+  // save the token as a state variable
+  String _accessToken;
 
   DataRepository({@required this.apiService});
 
   // use this method to get data for giving endpoint
   Future<int> getEndpointData(Endpoint endpoint) async {
-    final accessToken = await apiService.getAccessToken();
-    return await apiService.getEndpointData(
-        accessToken: accessToken, endpoint: endpoint);
+    try {
+      if (_accessToken == null) {
+        _accessToken = await apiService.getAccessToken();
+      }
+      return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+    } on Response catch (response) {
+      // if unauthorized, get access token again -> 401
+      if (response.statusCode == 401) {
+        _accessToken = await apiService.getAccessToken();
+        return await apiService.getEndpointData(
+          accessToken: _accessToken, endpoint: endpoint);
+      }
+      // if there is another error we just throw it
+      rethrow;
+    }
   }
+
+  // we must refresh the access token when it has beed expired
 }
