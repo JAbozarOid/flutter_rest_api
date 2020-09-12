a07fd97f353e03cf1dbf617f978a795b69ffdd4a
JAbozarOid <araghibdoust@hotmail.com>
2020-09-12 12:32:59 +0430

    endpoint_card

--- lib/ui/endpoit_card.dart
+++ lib/ui/endpoit_card.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rest_api/app/services/api.dart';
 
 class EndpointCard extends StatelessWidget {
+  final Endpoint endpoint;
+  final int value;
+
+  const EndpointCard({Key key, this.endpoint, this.value}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Cases',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              // for the checking the value is null or not there two way
+              // 1 this below line
+              Text(
+                value != null ? value.toString() : '',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              // 2 this below line
+              //Text(value?.toString() ?? '')
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
